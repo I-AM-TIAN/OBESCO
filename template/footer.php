65a3d80d2c2b5d4e41403b906d86cc1396729f78
I-AM-TIAN <105998755+I-AM-TIAN@users.noreply.github.com>
2023-09-28 22:07:36 -0500

    graficos de caracterizacion

--- template/footer.php
+++ template/footer.php
@@ -2,7 +2,7 @@
     <h1>ALIADOS</h1>
     <div class="aliados">
         <img src="../images/logos_aliados/SIEDDS.png" alt="">
-        <img src="../images/logos_aliados/ucc-.png" alt="">
+        <img src="../images/logos_aliados/ucc.png" alt="">
         <img src="../images/logos_aliados/indesco.png" alt="">
     </div>
     <div class="redes">
