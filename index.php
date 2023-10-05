fa36676bb51bfeb3e342818b7e860af6ae6b78dd
Académico <academico@CAMPUS.local>
2023-10-05 10:03:14 -0500

    graficos

--- index.php
+++ index.php
@@ -34,15 +34,15 @@
     <?php
     include("./template/options.php");
     ?>
-    <div class="texto">
+    <div id="info" class="texto">
       <h1>CONTENIDO</h1>
       <p>
         El sector de la economía solidaria se reconoce por su capacidad de autogestión, derivada del trabajo y los aportes de sus asociados; de esta manera, se potencian sus habilidades individuales y se logran aportes sustanciales en la calidad de vida de las personas y sus comunidades. Es así como se han realizado diferentes esfuerzos por medir el impacto socioeconómico de las organizaciones del sector, entre ellos se encuentra el reporte de estados financieros que deben realizar las organizaciones a la Supersolidaria (Superintendencia de Economía Solidaria).
         Ante esa necesidad, los gremios como Confecoop y Ascoop, la academia y los organismos internacionales han impulsado proyectos orientados a valorar tales impactos, mediante métodos cualitativos, cuantitativos o mixtos; también las organizaciones cada vez han incorporado modelos de rendición de cuentas buscando hacer visible el aporte que hacen a la sociedad; no obstante, el balance de este ejercicio indica que no han sido suficientes los estudios realizados, entre otras razones, porque cada uno define variables diferentes, se realizan en momentos del tiempo asimétricos y se usan instrumentos diversos, con lo cual se dificulta tener información comparable e impide aportar información a la toma de decisiones en las políticas públicas.
         Con respecto a dichas debilidades en la información y medición del impacto, se propuso en la Universidad Cooperativa de Colombia desarrollar el proyecto de investigación “Observatorio de impacto social, económico y ambiental de la economía solidaria en Colombia”. Teniendo como aspectos claves la publicación de resultados, que brinde información confiable a la comunidad, en general, academia, gremios, gobiernos, entre otros, razón por la cual, este portal web permite acceder a los recursos y los servicios del observatorio de economía solidaria, garantizando la conservación y la actualización permanente de la información.
-
       </p>
     </div>
+    <?php include("./reportes/rcaracterizacion.php") ?>
   </div>
   <script src="./controller/index.js"></script>
   <?php
---
+++ reportes/rcaracterizacion.php
@@ -0,0 +1,179 @@
+<?php
+// Conexión a la base de datos (reemplaza con tus credenciales y detalles de conexión)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "obesco_bdd";
+$port = "3312";
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Consulta SQL para obtener los datos
+$sql = "SELECT tipo_organizacion.tipo AS Tipo_Organizacion, COUNT(organizaciones.id_organizacion) AS Numero_de_Registros
+FROM tipo_organizacion
+INNER JOIN organizaciones ON tipo_organizacion.id_tipoorg = organizaciones.id_tipoorg
+GROUP BY tipo_organizacion.tipo;";
+
+$result = $conn->query($sql);
+
+// Crear un arreglo de datos para la gráfica
+$data = array();
+$data[] = ['Tipo de Organización', 'Número de Registros'];
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = [$row['Tipo_Organizacion'], (int)$row['Numero_de_Registros']];
+}
+
+
+//Grafico 2
+
+$sql2 = "SELECT sector_economico.Sector AS Sector_Economico, COUNT(organizaciones.id_organizacion) AS Numero_de_Registros
+FROM sector_economico
+INNER JOIN organizaciones ON sector_economico.idsec_eco = organizaciones.id_sectoreco
+GROUP BY sector_economico.Sector;";
+
+$result2 = $conn->query($sql2);
+
+// Crear un arreglo de datos para la gráfica
+$data2 = array();
+$data2[] = ['Sector Económico', 'Número de Registros'];
+
+while ($row2 = $result2->fetch_assoc()) {
+    $data2[] = [$row2['Sector_Economico'], (int)$row2['Numero_de_Registros']];
+}
+
+//Grafico 3
+
+$sql3 = "SELECT situacion_geografica.Situacion AS Situacion, COUNT(organizaciones.id_organizacion) AS Numero_de_Registros
+FROM situacion_geografica
+INNER JOIN organizaciones ON situacion_geografica.idsit_geogra = organizaciones.id_situaciongeo
+GROUP BY situacion_geografica.Situacion;";
+
+$result3 = $conn->query($sql3);
+
+// Crear un arreglo de datos para la gráfica
+$data3 = array();
+$data3[] = ['Situación Geográfica', 'Número de Registros'];
+
+while ($row3 = $result3->fetch_assoc()) {
+    $data3[] = [$row3['Situacion'], (int)$row3['Numero_de_Registros']];
+}
+
+//Grafico 4
+
+$sql4 = "SELECT ubicacion_geografica.Ubicacion AS Ubicacion, COUNT(organizaciones.id_organizacion) AS Numero_de_Registros
+FROM ubicacion_geografica
+INNER JOIN organizaciones ON ubicacion_geografica.idubi_geo = organizaciones.idubi_geo
+GROUP BY ubicacion_geografica.Ubicacion;";
+
+$result4 = $conn->query($sql4);
+
+// Crear un arreglo de datos para la gráfica
+$data4 = array();
+$data4[] = ['Ubicación geográfica', 'Número de Registros'];
+
+while ($row4 = $result4->fetch_assoc()) {
+    $data4[] = [$row4['Ubicacion'], (int)$row4['Numero_de_Registros']];
+}
+
+// Cerrar la conexión a la base de datos
+$conn->close();
+
+session_start();
+require_once "../../Config/conexion.php";
+$usuario = $_SESSION['id_user'];
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Incluir la biblioteca de Google Charts -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <link rel="stylesheet" href="./styles/rcaracterizacion" />
+</head>
+
+<body>
+    <!-- Elemento donde se dibujará la gráfica -->
+    <div id="graf" class="graficosc">
+        <div class= "grafico" id="grafico_1"></div>
+        <div class= "grafico" id="grafico_2"></div>
+        <div class= "grafico" id="grafico_3"></div>
+        <div class= "grafico" id="grafico_4"></div>
+    </div>
+    <script type="text/javascript">
+        // Cargar la biblioteca de visualización de Google Charts
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+
+        // Llamar a la función drawChart cuando la biblioteca esté lista
+        google.charts.setOnLoadCallback(grafico1);
+        google.charts.setOnLoadCallback(grafico2);
+        google.charts.setOnLoadCallback(grafico3);
+        google.charts.setOnLoadCallback(grafico4);
+
+        function grafico1() {
+            // Crear un objeto DataTable de Google Charts y agregar los datos
+            var data = google.visualization.arrayToDataTable(<?php echo json_encode($data); ?>);
+
+            // Configurar las opciones de la gráfica
+            var options = {
+                title: 'Tipos de organizaciones registradas',
+                pieHole: 0.4 // Agregar un agujero en el centro del gráfico (opcional)
+            };
+
+            // Crear la instancia de la gráfica y dibujar la gráfica circular en el elemento con el ID "chart_div"
+            var chart = new google.visualization.PieChart(document.getElementById('grafico_1'));
+            chart.draw(data, options);
+        }
+
+        function grafico2() {
+            // Crear un objeto DataTable de Google Charts y agregar los datos
+            var data2 = google.visualization.arrayToDataTable(<?php echo json_encode($data2); ?>);
+
+            // Configurar las opciones de la gráfica
+            var options = {
+                title: 'Sectores económicos',
+                pieHole: 0.4 // Agregar un agujero en el centro del gráfico (opcional)
+            };
+
+            // Crear la instancia de la gráfica y dibujar la gráfica circular en el elemento con el ID "chart_div"
+            var chart = new google.visualization.PieChart(document.getElementById('grafico_2'));
+            chart.draw(data2, options);
+        }
+
+        function grafico3() {
+
+            var data3 = google.visualization.arrayToDataTable(<?php echo json_encode($data3); ?>);
+
+            var options = {
+                title: 'Situaciones geográficas'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('grafico_3'));
+
+            chart.draw(data3, options);
+        }
+
+        function grafico4() {
+
+            var data4 = google.visualization.arrayToDataTable(<?php echo json_encode($data4); ?>);
+
+            var options = {
+                title: 'Ubicaciones geográficas'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('grafico_4'));
+
+            chart.draw(data4, options);
+        }
+    </script>
+</body>
+
+</html>
