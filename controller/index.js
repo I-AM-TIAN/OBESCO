a0c9e1f8b5f40b0a1f2da39d7181fbf4e4139a30
Académico <academico@CAMPUS.local>
2023-10-05 10:30:36 -0500

    graficos agregados en pagina principal

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,4 @@
 function mostrargra(){
     document.getElementById("info").classList.add("texto-hide");
-    document.getElementById("graf").classList.add("graficos-show");
+    document.getElementById("graf").classList.add("graficosc-show");
 }
