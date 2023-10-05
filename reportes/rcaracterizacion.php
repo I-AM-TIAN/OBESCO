9449c50c4480057bc62b236687f7008c9b5d85bd
Académico <academico@CAMPUS.local>
2023-10-05 10:04:14 -0500

    graficos

--- reportes/rcaracterizacion.php
+++ reportes/rcaracterizacion.php
@@ -4,7 +4,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "obesco_bdd";
-$port = "3312";
+$port = "3308";
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
